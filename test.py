16e4bfc1b2294bdef5347ebc94dd65b193655b2a
Oscar Evertsson <oscar.evertsson@live.com>
2018-10-22 10:21:15 +0200

    Add help parameter skeleton.

--- test.py
+++ test.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python
 import os
-cwd = os.getcwd()
+import sys
 
 
 def main():
-    print ("Hej")
-    print(cwd)
+    cwd = os.getcwd()
+    argument = sys.argv[1]
+
+    if argument:
+        if argument == "-h":
+            help()
+        else:
+            print("Unknown argument: %s" % argument)
+
+
+def help():
+    print(bcolors.WARNING + "Testest test test test"
+          + bcolors.ENDC)
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 if __name__ == "__main__":
