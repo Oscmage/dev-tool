02c621a78f7cf88572b3cd3768bf34fb6386579b
Oscar Evertsson <oscar.evertsson@live.com>
2018-11-05 08:06:52 +0100

    Rename.

--- devtool.py
+++ devtool.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python
 import os
 import sys
+import json
 
 
 def main():
     cwd = os.getcwd()
     handleArguments()
+    data = readDb()
+    print(data)
 
     # Get previous result from sonar-scanner
     # Run sonar-scanner with new code
     # Display simple comparison
+
+
+def writeResult(data):
+    with open('db.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+
+def readDb():
+    try:
+        with open('db.json', 'r') as f:
+            return json.load(f)
+    except OSError:
+        # Do nothing
+        print(bcolors.WARNING + "If this is not the first time running the super-mega-devtool on this project something went wrong" + bcolors.ENDC)
+        return {}
 
 
 def handleArguments():
